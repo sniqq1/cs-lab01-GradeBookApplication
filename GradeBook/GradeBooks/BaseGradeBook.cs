6be4349de2a28edcc289ccf3557565c98f5d3190
sniqq1 <pat.wojtaszek@gmail.com>
2020-11-04 10:02:36 +0100

    finish

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -110,21 +110,31 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double gpa = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    gpa= 4;
+                    break ;
                 case 'B':
-                    return 3;
+                    gpa= 3;
+                    break;
                 case 'C':
-                    return 2;
+                    gpa= 2;
+                    break;
                 case 'D':
-                    return 1;
+                    gpa = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    gpa = 0;
+                    break;
+            }
+            if (IsWeighted && (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled))
+            {
+                gpa += 1;
             }
            
-            return 0;
+            return gpa;
         }
 
         public virtual void CalculateStatistics()
--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,7 +41,7 @@ namespace GradeBook.UserInterfaces
             }*/
             if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
@@ -58,7 +58,7 @@ namespace GradeBook.UserInterfaces
             }
             else
             {
-                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
+                Console.WriteLine($"{type} is not a supported type of gradebook, please try again");
                 return;
             }
             //BaseGradeBook gradeBook = new BaseGradeBook(name);
@@ -89,7 +89,7 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
+            Console.WriteLine("Create 'Name' 'Type' 'Weighted' - Creates a new gradebook where 'Name' is the name of the gradebook, 'Type' is what type of grading it should use, and 'Weighted' is whether or not grades should be weighted (true or false).");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
